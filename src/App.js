12c1c771a04301502edacdf45d18599164aec2f6
mim2041 <suraiyamim338@gmail.com>
2023-03-26 22:56:11 +0600

    working from other file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Countries from './components/countries/Countries';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+      <Countries></Countries>
+      <Header></Header>
+      {/* <LoadCountries></LoadCountries> */}
     </div>
   );
 }
