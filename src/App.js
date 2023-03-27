b19b8c187013da664496a2c86faaa326e62a983c
mim2041 <suraiyamim338@gmail.com>
2023-03-27 22:51:45 +0600

    writting code from different components

--- src/App.js
+++ src/App.js
@@ -2,43 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 import Countries from './components/countries/Countries';
-import Header from './components/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
       <Countries></Countries>
-      <Header></Header>
-      {/* <LoadCountries></LoadCountries> */}
     </div>
   );
 }
 
-function LoadCountries(){
-  const [countries, setCountries] = useState([]);
-  useEffect(()=>{
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data => setCountries(data))
-  },[])
-  return (
-    <div>
-      <h1>Visiting every countries of the world!!!</h1>
-      <h3>Available Countries: {countries.length}</h3>
-      {
-        countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
-      }
-    </div>
-  )
-}
+// function LoadCountries(){
+//   const [countries, setCountries] = useState([]);
+//   useEffect(()=>{
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res => res.json())
+//     .then(data => setCountries(data))
+//   },[])
+//   return (
+//     <div>
+//       <h1>Visiting every countries of the world!!!</h1>
+//       <h3>Available Countries: {countries.length}</h3>
+//       {
+//         countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+//       }
+//     </div>
+//   )
+// }
 
-function Country(props){
-  return (
-    <div>
-      <h2>Name: {props.name}</h2>
-      <p>Population: {props.population}</p>
-    </div>
-  )
-}
+// function Country(props){
+//   return (
+//     <div>
+//       <h2>Name: {props.name}</h2>
+//       <p>Population: {props.population}</p>
+//     </div>
+//   )
+// }
 
 export default App;
--- src/components/countries/Countries.js
+++ src/components/countries/Countries.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Countries = () => {
+    const [countries, LoadCountries] = useState([]);
+    useEffect( () => {
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    },[])
     return (
         <div>
-            <h2>Countries in other File.</h2>
+            <h2>Hello from countries {countries.length}</h2>
         </div>
     );
 };
